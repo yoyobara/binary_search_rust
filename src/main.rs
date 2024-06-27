ccf6812a98556fed7b65803daed739bba5dc1090
yoyobara <yotambarak6@gmail.com>
2024-06-28 01:44:21 +0300

    seems ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::cmp::Ordering::*;
+
+/*
+ * my own generic binary search implementation... just for fun lol
+ */
+fn binary_search<T: Ord>(arr: &[T], value: &T) -> Option<usize> {
+    let mut start = 0;
+    let mut end = arr.len();
+
+    while end - start != 1 {
+        let firsthalf_end = start + (end - start) / 2;
+        match value.cmp(&arr[firsthalf_end]) {
+            Less => {
+                end = firsthalf_end;
+            }
+
+            Equal => {
+                return Some(firsthalf_end);
+            }
+
+            Greater => {
+                start = firsthalf_end;
+            }
+        }
+    }
+
+    if arr[start] == *value {
+        Some(start)
+    } else {
+        None
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(binary_search(&[2, 23, 52, 55], &52), Some(2));
 }
